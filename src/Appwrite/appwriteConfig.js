81fcac05321ae717d5cc177a498c2b0b478a1b78
kiranjeet kour <kiranjeetkour144@gmail.com>
2024-05-23 15:00:24 +0530

    done

--- src/Appwrite/appwriteConfig.js
+++ src/Appwrite/appwriteConfig.js
@@ -1,7 +1,9 @@
-import {Client,Account} from 'appwrite'
+import { Client, Account } from 'appwrite';
+
 const client = new Client();
-const AppwriteUrl = process.env.VITE_APPWRITE_KEY
-const ProjectUrl = process.env.VITE_APPWRITEPROJECT_KEY
+const AppwriteUrl = import.meta.env.VITE_APPWRITE_KEY;
+const ProjectUrl = import.meta.env.VITE_APPWRITEPROJECT_KEY;
+
 client
     .setEndpoint(AppwriteUrl)
     .setProject(ProjectUrl);
